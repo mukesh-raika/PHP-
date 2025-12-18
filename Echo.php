1e726721df9e34dd44e5372f1a7bcbb2cc448acc
mukesh <mukesh@gmail.com>
2025-12-18 18:29:30 +0530

    variables in php

--- Echo.php
+++ Echo.php
@@ -7,7 +7,7 @@ Echo 20+20;
 echo "<br>";
 echo "hello boy <br/>";
 echo "hello coder <br/>";
- echo "Age is 29 <br/>";
+ echo "Age is 29 <br/>"
 
  echo ("as  a function");
 ?>
---
+++ variables.php
@@ -0,0 +1,35 @@
+<?php
+$name="anil ";
+echo $name;
+echo "<br/>";
+
+echo $name ;
+echo "<br/>";
+
+echo $name;
+echo "<br/>";
+
+echo $name ;
+echo "<br/>";
+
+echo $name ;
+echo "<br/>";
+
+$num=20;
+echo $num*3;
+echo "<br/>";   
+
+
+echo $num*6;
+echo "<br/>";  
+
+echo $num*7;
+echo "<br/>";  
+
+echo $num*30;
+echo "<br/>";  
+
+$info="1.1";
+echo "$info"
+  
+?>
