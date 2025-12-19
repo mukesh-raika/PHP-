652d2f1f3791b66f4965dcba9a70b1a063c571d8
mukesh <mukesh@gmail.com>
2025-12-19 12:20:43 +0530

    constants.  in php

---
+++ Constants.php
@@ -0,0 +1,16 @@
+ <!-- constants ko do type se lhik  sakte he  -->
+<?php
+// type 1
+echo "<h1>Constants.php</h1>";
+const data ="<h2 style='color:Purple'>im a web Developer</h2>";
+echo data;
+
+?>
+ 
+ <!-- type 2 -->
+<?php
+define ("DATA","<h2 style='color:red'>test2</h2>");
+echo DATA;
+// define ("data","test1");
+
+?>  
