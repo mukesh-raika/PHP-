cb3a36ca5a9fc10c5a1a4fb96321005e3d834cc7
mukesh <mukesh@gmail.com>
2025-12-19 17:40:54 +0530

    if-else

---
+++ if-else.php
@@ -0,0 +1,18 @@
+<?php
+$age=20; 
+
+if($age>18){
+    echo "<h1 style='color:purple'>welcome to website<h1/>";
+}else{
+    echo "<h1 style='color:red'>you are under age<h1/>";
+}
+
+$name="coder boy";
+
+if($name=="coder "){
+    echo "hi coder boy";
+}else{
+    echo "<h1 style='color:red'>this user is not present is our Database";
+}
+
+?>
