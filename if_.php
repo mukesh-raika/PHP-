2afb385353da3596226357133bece68bdea9c2bf
mukesh <mukesh@gmail.com>
2025-12-19 17:21:41 +0530

    if

---
+++ if_.php
@@ -0,0 +1,20 @@
+<?php
+$age=20;
+
+$name="hi coder boy";
+
+$info="hi ninja coder";
+
+if($age>18){
+    echo "<h1 style='color:red'>Display kids content<h1/>";
+}
+// echo "<br/>";
+if($name=="hi coder boy"){
+    echo "<h1 style='color:blue'>Welcome coder boy</h1>";
+}   
+// echo "<br/>";
+if($info=="hi ninja coder"){
+    echo "<h1 style='color:purple'>welcome ninja coder</h1>";
+}
+echo "<h1 style='color:orange'>other<h1/>";
+?>
