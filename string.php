e4ebc1db1b18aa938ab93050392a8df133af3d66
mukesh <mukesh@gmail.com>
2025-12-19 16:39:17 +0530

    strings and string

---
+++ string.php
@@ -0,0 +1,18 @@
+<?php
+$name ="info ";
+$age=18;
+echo "<h1 style='color:red'>my name is coder boy $name </h1>"; 
+echo "<br>";
+
+echo '<h2 style="color:blue"> hello ninja coder </h2>'.$name;
+echo "<br>";
+
+echo 'my name '.$name.   ' and my age is'.$age;
+echo "<br>";
+
+$str="Hello How are you";
+$str1="My name is anil sidhu";
+echo $str .$str1;
+
+
+?>
