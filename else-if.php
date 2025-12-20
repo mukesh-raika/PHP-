667182a49899c2174e5e705b85d05830b38efecd
mukesh <mukesh@gmail.com>
2025-12-20 10:28:51 +0530

    else if in php

---
+++ else-if.php
@@ -0,0 +1,14 @@
+<?php
+
+$country="India";
+if($country=="India"){
+    echo "this is an asin India country"; 
+}elseif($country=="france"){
+    echo "this is an european country";
+}elseif($country=="Canada"){
+    echo "this is an north americn country";
+}
+else{
+    echo " Unknown";
+}
+?>
