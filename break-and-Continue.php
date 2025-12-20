05386b835147d34fd5d19f53c9455817f9cedd03
mukesh <mukesh@gmail.com>
2025-12-20 12:47:51 +0530

    break-and-continue

---
+++ break-and-Continue.php
@@ -0,0 +1,18 @@
+<?php
+$user_need=5;
+for($i=1;$i<=10;$i++){
+    echo $i;
+    echo "<br>";
+if($i==$user_need){
+    break;
+}
+}
+echo "<br>";
+for($i=0;$i<=10;$i++){
+    if($i==3 || $i==7){
+        continue;
+    }
+    echo $i;
+    echo "<br>";
+}
+ ?>
