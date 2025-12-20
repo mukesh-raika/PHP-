908b6391b48819e4d2da20605dc9189be991c3cc
mukesh <mukesh@gmail.com>
2025-12-20 12:16:59 +0530

    for loop

---
+++ for-loop.php
@@ -0,0 +1,28 @@
+<?php
+$table=18;
+$table1=19;
+$table2=24;
+$table3=25;
+for($start=1;$start<=10;$start++){
+    echo  $table*$start;
+    echo "<br>";
+  
+}
+echo "<br>";
+
+for($m=1;$m<=10;$m++){
+    echo  $table1*$m ;
+    echo "<br>";
+  
+}
+echo "<br>";
+ for($i=1;$i<=10;$i++){
+    echo $table2*$i;
+    echo "<br>";
+ }
+echo "<br>";
+ for($m=1;$m<=10;$m++){
+    echo $table3*$m;
+    echo "<br>";
+ }
+?>
