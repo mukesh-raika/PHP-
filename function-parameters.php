65968d6b13c1dcda33f575419c699dd0df0b36ca
mukesh <mukesh@gmail.com>
2025-12-20 17:33:28 +0530

    function parameter

---
+++ function-parameters.php
@@ -0,0 +1,21 @@
+<?php
+
+function info($a,$b){
+    echo $a+$b;
+    echo "<br>";
+}
+
+info(20,50);
+info(18,30);
+info(26,25);
+info(21,51);
+
+
+function inf($s,$m,){
+    echo  "<h1 style='color:blue'>$s,$m<h1/>";
+    
+}
+inf("mukesh","raika");
+inf("<h1 style='color:purple'>mukesh","raika<h1/>");
+inf("<h1 style='color:green'>coder","boy<h1/>");
+?>
