29db0a16bfdaa7e497234be3b1eb40fc2414cf97
mukesh <mukesh@gmail.com>
2025-12-22 17:03:13 +0530

    multidimensional array

---
+++ multidimensional-array.php
@@ -0,0 +1,18 @@
+<?php
+
+$users=[
+    [1,"anil","noida","anil@test.com"],
+    [2,"sam","delhi","sam@test.com"],
+    [3,"peter","gurgaon","peter@test.com"],
+];
+// echo "<pre>";
+// print_r($users);
+// echo "<pre>";
+for($i=0;$i<count($users);$i++){
+    // print_r($users[$i]);
+    for($j=0;$j<count($users[$i]);$j++){
+        echo $users[$i][$j];
+        echo  "<br>";
+    }
+}
+?>
