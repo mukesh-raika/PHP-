add2aefff16ae84cdd6f096b5390886c3fc504fa
mukesh <mukesh@gmail.com>
2025-12-22 12:10:22 +0530

    importent function

---
+++ importent-functions.php
@@ -0,0 +1,31 @@
+<?php
+$str="hi coder boy ";
+echo strlen($str);
+echo "<br>";
+$var ="aplle";
+var_dump($var);
+echo "<br>";
+echo date("d");
+echo date("l");
+echo date("M");
+echo date("m.d.y");
+echo "<br>";
+// echo phpinfo();
+
+$ar ="abc";
+echo is_string($ar);
+echo "<br>";
+$inf =123;
+echo is_int($inf);
+echo "<br>";
+echo rand();
+echo "<br>";
+echo "before";
+// die();
+// exit();
+echo "after";
+echo "<br>";
+
+$str="code step by stap";
+echo substr($str,5)
+?>
