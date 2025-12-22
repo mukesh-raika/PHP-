cf9c471bf636d5ab3a6a852f8c0f540a9fc67c2a
mukesh <mukesh@gmail.com>
2025-12-22 13:20:43 +0530

    array

---
+++ array.php
@@ -0,0 +1,13 @@
+<?php
+ $user=array("<h1 style='color:blue'>coder boy<h1/>","sam","peter","bruce");
+ echo $user[0];
+ echo "<br>";
+
+
+$users = ["anil","sidhu","coder"];
+
+foreach($users as $user){
+    echo $user . "<br>";
+}
+?>
+
