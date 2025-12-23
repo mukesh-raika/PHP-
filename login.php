4da3e243c9385313151d7b855344dd28422afdea
mukesh <mukesh@gmail.com>
2025-12-23 15:07:03 +0530

    request post

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+// echo "this is php login file";
+if($_GET){
+echo "user name is ".$_GET["user_name"];
+echo "<br>";
+echo "user paasword is ".$_GET["user_password"];
+echo "<br>";
+echo "user email is ".$_GET["user_email"];
+}
+?>
