d5c0974d2a2651c5f94b0e39614cd4925b87a8a7
mukesh <mukesh@gmail.com>
2025-12-26 12:16:22 +0530

    cookies

---
+++ blog/cookies.php
@@ -0,0 +1,18 @@
+<?php
+
+setcookie("friut","apple",time()+(86400));
+setcookie("color","red",time()+86400);
+ if(isset($_COOKIE['friut'])){
+    echo 'curent friut is '.($_COOKIE['friut']);
+ }else{
+    echo "no cookie set";
+ }
+echo "<br>";
+ if(isset($_COOKIE['color'])){
+    echo  'curent color is ' .($_COOKIE['color']);
+ }else{
+    echo "no color set";
+ }
+
+ 
+?>
