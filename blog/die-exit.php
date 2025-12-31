88e7b0877d712a689d8508dd2742b371b01da7ef
mukesh <mukesh@gmail.com>
2025-12-31 17:38:25 +0530

    die-exit-return

---
+++ blog/die-exit.php
@@ -0,0 +1,24 @@
+<?php
+$a=10;
+$b=20;
+echo $a-$b;
+echo "<br>";
+// exit("1st print echo");
+echo $a+$b;
+echo "<br>";
+echo test();
+echo "<br>";
+// die("2echo print");
+echo $a*$b;
+echo "<br>";
+
+echo $a/$b;
+echo "<br>";
+
+function test(){
+    return 80;
+echo  "after 20";
+
+}
+
+?>
