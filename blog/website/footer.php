0b3b5a9cf19c66a3cd991230594c273462c0f9a1
mukesh <mukesh@gmail.com>
2025-12-31 16:06:38 +0530

    website

---
+++ blog/website/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">about</a></li>
+    <li><a href="cotact.php">contact</a></li>
+ </ul>
+</footer>
---
+++ blog/website/header.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <h3>
+        LOGO
+    </h3>    
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="cotact.php">Contact</a></li>   
+    </ul>
+</div>
