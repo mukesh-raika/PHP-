7687ac48d94047f344c47f674617432bec1131cb
mukesh <mukesh@gmail.com>
2026-01-05 16:11:53 +0530

    mysql pdo database php

---
+++ blog/mysql-pdo.php
@@ -0,0 +1,14 @@
+<?php
+    $host="localhost";
+    $username="root";
+    $password=null;
+    
+    try{
+        $conn=new PDO("mysql:host=$host;dbname=college",$username,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo "connect done";
+    }catch(PDOException $err){
+     echo "connect failed $err->getMessage()"; 
+    }
+
+?>  
