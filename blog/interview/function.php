3ca668d9b12b4b67cec9958a90191beabf563c25
mukesh <mukesh@gmail.com>
2026-01-09 17:01:26 +0530

    function

---
+++ blog/interview/function.php
@@ -0,0 +1,36 @@
+<?php
+function getName() {
+    return "Mukesh";
+}
+echo getName();
+echo "<br>";
+echo getName();
+
+
+
+function outerFunction() {
+
+    function innerFunction() {
+        echo "Hello";
+    }
+
+}
+
+outerfunction();
+innerfunction();
+
+
+echo "<br>";
+
+function getUserName($name, $age) {
+    return $name;  
+}
+
+function welcomeUser($a, $b) {
+    $name = getUserName($a, $b);
+    return "Welcome " . $name;
+}
+
+echo welcomeUser("Mukesh", 18);
+?>
+
