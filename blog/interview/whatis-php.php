cf2f99e962a80717c5ffede4b597d5b1c77e7400
mukesh <mukesh@gmail.com>
2026-01-09 15:12:59 +0530

    echo nad print diffres

---
+++ blog/interview/whatis-php.php
@@ -0,0 +1,13 @@
+<?php
+echo "hello world";
+echo "<br>";
+echo "hello";
+echo "<br>";
+echo "world";
+echo "<br>";
+print "hello world";
+echo "<br>";
+print "mukesh";
+echo "<br>";
+print "raika";
+?>
---
+++ blog/opps/class-object.php
@@ -0,0 +1,15 @@
+<?php
+class MathOperations{
+function sum(){
+    echo 10+20;
+}
+}
+
+$maths= new MathOperations();
+
+ $maths->sum();
+ echo "<br>";
+ $maths->sum();
+
+
+?>
