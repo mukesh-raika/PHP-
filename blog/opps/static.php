ace617a696260ef792eeb7c7460023694f53ba88
mukesh <mukesh@gmail.com>
2026-01-13 15:55:08 +0530

    static

---
+++ blog/opps/static.php
@@ -0,0 +1,18 @@
+<?php
+class Honda{
+    static public $countryName="<h3>japan</h3>";
+    static function companyName(){
+        echo "<h1 style='color:blue'>Honda</h1>";
+    }
+
+    
+}
+Honda::companyName();
+echo Honda::$countryName;
+
+
+
+class Hf{
+    
+}
+?>
